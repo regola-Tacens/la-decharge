44b908bbfdf495b4dbd1018ec9f006918b00ce11
regola-Tacens <regolas_tacens@outlook.fr>
2021-08-26 07:32:36 +0200

    implementation envoi des infos vers chaque page article

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,4 +1,43 @@
-export default function DetailArticle() {
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space : process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY ,
+})
+
+export const getStaticPaths = async () => {
+    const res = await client.getEntries({
+      content_type :'article'
+    })
+
+    const paths = res.items.map(item => {
+      return {
+        params:{ slug : item.fields.slug}
+      }
+    })
+
+    return {
+      paths:paths,
+      fallback :false
+    }
+}
+
+export async function getStaticProps ( { params }){
+
+    const { items } = await client.getEntries({
+        content_type :'article',
+        'fields.slug' : params.slug
+    })
+
+    return {
+      props : { article : items[0]}
+    }
+}
+
+
+
+export default function DetailArticle( { article }) {
+  console.log(article)
   return (
     <div>
       Article
