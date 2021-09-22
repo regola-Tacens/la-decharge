0713d7eb3c842447dea0fbcce195b2bcd18f6dd0
regola-Tacens <regolas-tacens@outlook.fr>
2021-09-22 22:51:57 +0200

    ajout d'effet au survol des liens

--- components/Layout.js
+++ components/Layout.js
@@ -6,10 +6,10 @@ export default function Layout({ children }) {
       <header>
         <nav>
           <div className="header">
-            <Link href="/"><a><h1>la Décharge</h1></a></Link>
+            <Link href="/"><a><h1>? la Décharge</h1></a></Link>
             <div className="right">
-              <Link href="/proprio"><a>le proprio</a></Link>
-              <Link href="/"><a>les voisins</a></Link>
+              <Link href="/proprio"><a className="menulink menulink-mneme">le proprio</a></Link>
+              <Link href="/"><a className="menulink menulink-mneme">les voisins</a></Link>
             </div>
           </div>
         </nav>
@@ -26,6 +26,71 @@ export default function Layout({ children }) {
           <p>plan site</p>
         </div>
       </footer>
+      <style JSX>{`
+                .menulink{
+                  cursor: pointer; 
+                  text-decoration:none;
+                  color:black;
+                  position: relative;
+                }
+                .menulink::before,
+                .menulink::after {
+                    position: absolute;
+                    width: 100%;
+                    height: 1px;
+                    background: black;
+                    top: 100%;
+                    left: 0;
+                    pointer-events: none;
+                }
+                .menulink::before {
+                  content: '';
+                }
+                .menulink-mneme {
+                  font-family: aktiv-grotesk-extended, sans-serif;
+                  font-size: 0.875rem;
+                  text-transform: uppercase;
+                }
+              
+                .menulink-mneme::before {
+                    height: 100%;
+                    top: 0;
+                    opacity: 0;
+                }
+                .menulink-mneme:hover::before {
+                  opacity: 1;
+                  animation: coverUp 0.3s ease forwards;
+                }
+                @keyframes coverUp {
+                  0% {
+                      transform-origin: 50% 100%;
+                      transform: scale3d(1, 0.045, 1);
+                  }
+              
+                  50% {
+                      transform-origin: 50% 100%;
+                      transform: scale3d(1, 1, 1);
+                  }
+              
+                  51% {
+                      transform-origin: 50% 0%;
+                      transform: scale3d(1, 1, 1);
+                  }
+              
+                  100% {
+                      transform-origin: 50% 0%;
+                      transform: scale3d(1, 0.045, 1);
+                  }
+                }
+                .menulink-mneme::after {
+                  content: '';
+                  transition: opacity 0.3s;
+                }
+              
+                .menulink-mneme:hover::after {
+                    opacity: 0;
+                }
+            `}</style>
     </div>
   )
 }
--- components/articleCard.js
+++ components/articleCard.js
@@ -15,16 +15,17 @@ export default function ArticleCard({article}) {
                 height={thumbnail.fields.file.details.image.height}
                 />
             </div>
-            <div className='content'>
-                <h4>{title}</h4>
-                {/* <p>{texteArticle}</p> */}
-            </div>
-            <div className='action'>
-                <Link href={`/articles/${slug}`}><a>vers l'article</a></Link>
+            <div className="article_infos">
+              <div className='content'>
+                  <h4>{title}</h4>
+              </div>
+              <div className='action'>
+                  <Link href={`/articles/${slug}`}><a className="link link--io">vers l'article</a></Link>
+              </div>
             </div>
 
             <style JSX>{`
-                .article_container{
+                .article_container{ 
                     padding:5%;
                     width : 40%;
                     display : flex;
@@ -33,13 +34,65 @@ export default function ArticleCard({article}) {
                 .thumbnail{
                     width:50%;
                     object-fit : contain;
+                    transition: 0.2s ease-out;
+                }
+                .thumbnail:hover{
+                  transform:scale(1.03);
+                  transition: 0.3s ease-in;
+                }
+                
+                h4{
+                  margin-top:0.2em;
+                  margin-bottom:0.4em;
+                  font-size:1.3em;
                 }
                 @media only screen and (max-width: 800px) {
                     .article_container{
                       width:90%;
                       align-items: center;
                     }
-                  }
+                }
+                .link{
+                  cursor: pointer; 
+                  text-decoration:none;
+                  color:black;
+                  position: relative;
+                }
+                .link::before,
+                .link::after {
+                    position: absolute;
+                    width: 100%;
+                    height: 1px;
+                    background: black;
+                    top: 100%;
+                    left: 0;
+                    pointer-events: none;
+                }
+                .link::before {
+                  content: '';
+                }
+                .link--io::before {
+                  transform-origin: 100% 50%;
+                  transform: scale3d(0, 1, 1);
+                  transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+                }
+                .link--io:hover::before {
+                  transform-origin: 0% 50%;
+                  transform: scale3d(1, 1, 1);
+                  transition-timing-function: cubic-bezier(0.4, 1, 0.8, 1);
+                }
+                .link--io::after {
+                  content: '';
+                  top: calc(100% + 4px);
+                  transform-origin: 0% 50%;
+                  transform: scale3d(0, 1, 1);
+                  transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+                 }
+                .link--io:hover::after {
+                    transform-origin: 100% 50%;
+                    transform: scale3d(1, 1, 1);
+                    transition-timing-function: cubic-bezier(0.4, 1, 0.8, 1);
+                }
                 
             `}</style>
         </div>
