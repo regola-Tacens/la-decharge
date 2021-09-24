f655ebe0ffe1f8975b77258e2d72eba90bf04435
regola-Tacens <regolas-tacens@outlook.fr>
2021-09-24 16:15:16 +0200

    ajout de balise meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
     <Layout>
+    <Head>
+      <title>le décharge</title>
+      <meta property='og:title' content='Title of the article'/>
+      <meta property='og:image' content='/decharge.jpg'/>
+      <meta property='og:description' content='blog perso'/>
+    </Head>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
